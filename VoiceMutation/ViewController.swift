fa0fb8cafecacd53b7620bb9277a8b474ecbfa3b
MagmMe <marcin@magm.me>
2020-05-14 06:47:33 +0200

    Initial Commit

---
+++ VoiceMutation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceMutation
+//
+//  Created by Marcin Magiera on 14/05/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
