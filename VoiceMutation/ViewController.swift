c445b8e82e5f90ca9a3ed8a3e1d1ecaffe1cf806
MagmMe <marcin@magm.me>
2020-05-19 18:45:53 +0200

    adding label nad buttons

--- VoiceMutation/ViewController.swift
+++ VoiceMutation/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        
+        recordingLabel.text = "Nagrywam"
+    }
+    
 }
 
