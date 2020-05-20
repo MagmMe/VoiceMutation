610f33d68386055871ebb492d7329cfee8b2a2b6
MagmMe <marcin@magm.me>
2020-05-20 07:49:49 +0200

    adding assets

--- VoiceMutation/ViewController.swift
+++ VoiceMutation/ViewController.swift
@@ -11,17 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidApear")
+    }
 
     @IBAction func recordAudio(_ sender: Any) {
         
         recordingLabel.text = "Nagrywam"
+        recordingLabel.textColor = UIColor.red
     }
     
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Wciśnij, by zacząć nagrywać"
+        recordingLabel.textColor = UIColor.black
+    }
 }
 
